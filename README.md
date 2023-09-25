c90eaa10170796b03564cf09d8931f7d2e8ec75a
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-09-25 16:00:23 +0200

    fix: `poly_operations`

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # zk-fhe
 Zk proving the correct execution of encryption operation under BFV Fully Homomorphic Encryption scheme
+
+### TO DOs
+
+// - [ ] Add assumptions for each chip
+// - [ ] Add master example to test the whole encryption/decryption flow
+// - [ ] Avoid overflow in poly addition and poly multiplication in the circuit level 
+
--- src/chips/poly_operations.rs
+++ src/chips/poly_operations.rs
@@ -4,30 +4,22 @@ use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
 use halo2_base::AssignedValue;
 use halo2_base::Context;
 
-// TO DO:
-// - Add assumptions
-// - Add tests
-// - Add documentation
-// - Assert that the polynomials are of the same degree
-// - Work with polynomials at the example level
-// - Specify what is Q in poly lib
-
 // Build the sum of the polynomials a and b as sum of the coefficients
-fn poly_add(
+// N is the degree of the polynomial
+fn poly_add<const N: u64>(
     ctx: &mut Context<Fr>,
     a: Vec<AssignedValue<Fr>>,
     b: Vec<AssignedValue<Fr>>,
     gate: &GateChip<Fr>,
 ) -> Vec<AssignedValue<Fr>> {
-    let degree = a.len() - 1;
-
-    // assert that the polynomials are of the same degree
-    assert_eq!(degree, b.len() - 1);
+    // assert that the input polynomials have the same degree and this is equal to N
+    assert_eq!(a.len() - 1, b.len() - 1);
+    assert_eq!(a.len() - 1, N as usize);
 
     let c: Vec<AssignedValue<Fr>> = a
         .iter()
         .zip(b.iter())
-        .take(2 * degree - 1)
+        .take(2 * (N as usize) - 1)
         .map(|(&a, &b)| gate.add(ctx, a, b))
         .collect();
 
@@ -35,34 +27,33 @@ fn poly_add(
 }
 
 // Build the product of the polynomials a and b as dot product of the coefficients of a and b
-fn poly_mul(
+fn poly_mul<const N: u64>(
     ctx: &mut Context<Fr>,
     a: Vec<AssignedValue<Fr>>,
     b: Vec<AssignedValue<Fr>>,
     gate: &GateChip<Fr>,
 ) -> Vec<AssignedValue<Fr>> {
-    let degree = a.len() - 1;
-
-    // assert that the polynomials are of the same degree
-    assert_eq!(degree, b.len() - 1);
+    // assert that the input polynomials have the same degree and this is equal to N
+    assert_eq!(a.len() - 1, b.len() - 1);
+    assert_eq!(a.len() - 1, N as usize);
 
     let c: Vec<AssignedValue<Fr>> = vec![gate.mul(ctx, a[0], b[0])];
 
     let mut prod_val: Vec<AssignedValue<Fr>> = vec![];
 
-    for i in 0..(2 * degree + 1) {
+    for i in 0..(2 * (N + 1)) {
         let mut coefficient_accumaltor: Vec<AssignedValue<Fr>> = vec![];
 
-        if i < degree + 1 {
+        if i < (N + 1) {
             for a_idx in 0..=i {
-                let a_coef = a[a_idx];
-                let b_coef = b[i - a_idx];
+                let a_coef = a[a_idx as usize];
+                let b_coef = b[(i - a_idx) as usize];
                 coefficient_accumaltor.push(gate.mul(ctx, a_coef, b_coef));
             }
         } else {
-            for a_idx in (i - degree)..=degree {
-                let a_coef = a[a_idx];
-                let b_coef = b[i - a_idx];
+            for a_idx in (i - N)..=N {
+                let a_coef = a[a_idx as usize];
+                let b_coef = b[(i - a_idx) as usize];
                 coefficient_accumaltor.push(gate.mul(ctx, a_coef, b_coef));
             }
         }
@@ -79,12 +70,15 @@ fn poly_mul(
 }
 
 // Build the scalar multiplication of the polynomials a and the scalar k as scalar multiplication of the coefficients of a and k
-fn poly_scalar_mul(
+fn poly_scalar_mul<const N: u64>(
     ctx: &mut Context<Fr>,
     a: Vec<AssignedValue<Fr>>,
     b: AssignedValue<Fr>,
     gate: &GateChip<Fr>,
 ) -> Vec<AssignedValue<Fr>> {
+    // assert that the degree of the polynomial a is equal to N
+    assert_eq!(a.len() - 1, N as usize);
+
     let c: Vec<AssignedValue<Fr>> = a.iter().map(|&a| gate.mul(ctx, a, b)).collect();
 
     c
