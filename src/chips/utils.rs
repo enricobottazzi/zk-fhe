e4ad7a66576564020b58f73aa8c9dffd00e9f6d9
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-10-05 10:05:24 +0200

    fix: `poly_reduce` and `poly_divide_by_cyclo`

--- src/chips/utils.rs
+++ src/chips/utils.rs
@@ -1,18 +1,17 @@
+use halo2_base::{utils::ScalarField, AssignedValue};
 
-
-
-pub fn div_euclid(dividend: &Vec<u64>, divisor: &Vec<u64>) -> (Vec<u64>, Vec<u64>) {
+/// Performs long polynomial division on two polynomials
+/// Returns the quotient and remainder
+/// Input polynomials are parsed as a vector of assigned coefficients [a_DEG, a_DEG-1, ..., a_1, a_0] where a_0 is the constant term
+/// Q is the modulus of the Ring. All the coefficients will be in the range [0, Q-1]
+pub fn div_euclid<const Q: u64>(dividend: &Vec<u64>, divisor: &Vec<u64>) -> (Vec<u64>, Vec<u64>) {
     if divisor.is_empty() || divisor.iter().all(|&x| x == 0) {
         panic!("Cannot divide by a zero polynomial!");
     }
 
     // transform the dividend and divisor into a vector of i64
     let mut dividend = dividend.iter().map(|&x| x as i64).collect::<Vec<i64>>();
-    let mut divisor = divisor.iter().map(|&x| x as i64).collect::<Vec<i64>>();
-
-    // reverse the dividend and divisor
-    dividend.reverse();
-    divisor.reverse();
+    let divisor = divisor.iter().map(|&x| x as i64).collect::<Vec<i64>>();
 
     let mut quotient = Vec::new();
     let mut remainder = Vec::new();
@@ -50,14 +49,28 @@ pub fn div_euclid(dividend: &Vec<u64>, divisor: &Vec<u64>) -> (Vec<u64>, Vec<u64
     }
 
     // Convert remainder back to u64
-    let mut remainder = remainder.iter().map(|&x| x as u64).collect::<Vec<u64>>();
+    let remainder = remainder.iter().map(|&x| x as u64).collect::<Vec<u64>>();
 
     // Convert quotient back to u64
-    let mut quotient = quotient.iter().map(|&x| x as u64).collect::<Vec<u64>>();
-
-    // reverse the quotient and remainder
-    quotient.reverse();
-    remainder.reverse();
+    let quotient = quotient.iter().map(|&x| x as u64).collect::<Vec<u64>>();
 
     (quotient, remainder)
+}
+
+/// Convert a vector of AssignedValue to a vector of u64
+/// Assumes that each element of AssignedValue can be represented in 8 bytes
+pub fn vec_assigned_to_vec_u64<F: ScalarField>(vec: &Vec<AssignedValue<F>>) -> Vec<u64> {
+    let mut vec_u64 = Vec::new();
+
+    for i in 0..vec.len() {
+        let value_bytes_le = vec[i].value().to_bytes_le();
+
+        // slice value_to_bytes_le the first 8 bytes
+        let value_8_bytes_le = &value_bytes_le[..8];
+        let mut array_value_8_bytes_le = [0u8; 8];
+        array_value_8_bytes_le.copy_from_slice(value_8_bytes_le);
+        let num = u64::from_le_bytes(array_value_8_bytes_le);
+        vec_u64.push(num);
+    }
+    vec_u64
 }
