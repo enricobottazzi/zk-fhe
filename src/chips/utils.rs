cd2ace9dc1ce15e7b9c83c5e6b3ebb8de7188666
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-11-23 14:29:21 +0100

    feat: update `utils`

--- src/chips/utils.rs
+++ src/chips/utils.rs
@@ -1,6 +1,7 @@
 use halo2_base::{utils::ScalarField, AssignedValue};
-use num_bigint::BigUint;
+use num_bigint::BigInt;
 use num_traits::identities::Zero;
+use num_traits::Num;
 
 /// Performs long polynomial division on two polynomials
 /// Returns the quotient and remainder
@@ -88,6 +89,70 @@ pub fn div_euclid<const DEG_DVD: usize, const DEG_DVS: usize, const Q: u64>(
     (quotient, remainder)
 }
 
+/// Performs long polynomial division on two polynomials
+/// Returns the quotient and remainder
+///
+/// * Input polynomials are parsed as a vector of assigned coefficients [a_DEG, a_DEG-1, ..., a_1, a_0] where a_0 is the constant term
+/// * DEG_DVD is the degree of the dividend
+/// * DEG_DVS is the degree of the divisor
+/// * F is the scalar field of the circuit
+pub fn div_euclid_big_int<const DEG_DVD: usize, const DEG_DVS: usize, F: ScalarField>(
+    dividend: &Vec<BigInt>,
+    divisor: &Vec<BigInt>,
+) -> (Vec<BigInt>, Vec<BigInt>) {
+    if divisor.is_empty() || divisor.iter().all(BigInt::is_zero) {
+        panic!("Cannot divide by a zero polynomial!");
+    }
+    if dividend.is_empty() || dividend.iter().all(BigInt::is_zero) {
+        return (
+            vec![BigInt::zero(); DEG_DVD - DEG_DVS + 1],
+            vec![BigInt::zero(); DEG_DVS],
+        );
+    }
+
+    assert_eq!(dividend.len() - 1, DEG_DVD);
+    assert_eq!(divisor.len() - 1, DEG_DVS);
+
+    let mut dividend = dividend.clone();
+    let divisor = divisor.clone();
+    let mut quotient: Vec<BigInt> = Vec::new();
+    let mut remainder: Vec<BigInt> = Vec::new();
+
+    while dividend.len() > divisor.len() - 1 {
+        let leading_coefficient_ratio = &dividend[0] / &divisor[0];
+        quotient.push(leading_coefficient_ratio.clone());
+
+        for (i, coeff) in divisor.iter().enumerate() {
+            dividend[i] = &dividend[i] - &(&leading_coefficient_ratio * coeff);
+        }
+
+        dividend.remove(0);
+    }
+
+    for coeff in &dividend {
+        remainder.push(coeff.clone());
+    }
+
+    while !quotient.is_empty() && quotient[0].is_zero() {
+        quotient.remove(0);
+    }
+
+    while !remainder.is_empty() && remainder[0].is_zero() {
+        remainder.remove(0);
+    }
+
+    // Range over remainder. If any element is negative, returns the corresponding positive field element
+    for coeff in &mut remainder {
+        if *coeff < BigInt::zero() {
+            let modulus_big_int = BigInt::from_str_radix(&F::MODULUS[2..], 16).unwrap();
+
+            *coeff += &modulus_big_int;
+        };
+    }
+
+    (quotient, remainder)
+}
+
 /// Convert a vector of AssignedValue to a vector of u64
 ///
 /// * Assumes that each element of AssignedValue can be represented in 8 bytes
@@ -107,28 +172,28 @@ pub fn vec_assigned_to_vec_u64<F: ScalarField>(vec: &Vec<AssignedValue<F>>) -> V
     vec_u64
 }
 
-/// Convert a vector of u64 to a vector of BigUint
-pub fn vec_u64_to_vec_big_uint(vec: &Vec<u64>) -> Vec<BigUint> {
-    let mut vec_big_uint = Vec::new();
+/// Convert a vector of u64 to a vector of BigInt
+pub fn vec_u64_to_vec_big_int(vec: &Vec<u64>) -> Vec<BigInt> {
+    let mut vec_big_int = Vec::new();
 
     for i in 0..vec.len() {
-        vec_big_uint.push(BigUint::from(vec[i]));
+        vec_big_int.push(BigInt::from(vec[i]));
     }
-    vec_big_uint
+    vec_big_int
 }
 
 /// Performs polynomial multiplication on two polynomials using direct method
 /// Returns the product of the input polynomials
 ///
-/// * Input polynomials are parsed as a vector of BigUint coefficients [a_DEG, a_DEG-1, ..., a_1, a_0]
+/// * Input polynomials are parsed as a vector of BigInt coefficients [a_DEG, a_DEG-1, ..., a_1, a_0]
 ///   where a_0 is the constant term
-pub fn poly_mul(a: &Vec<BigUint>, b: &Vec<BigUint>) -> Vec<BigUint> {
+pub fn poly_mul(a: &Vec<BigInt>, b: &Vec<BigInt>) -> Vec<BigInt> {
     let deg_a = a.len() - 1;
     let deg_b = b.len() - 1;
     let deg_c = deg_a + deg_b;
 
     // initialize the output polynomial with zeroes
-    let mut c = vec![BigUint::zero(); deg_c + 1];
+    let mut c = vec![BigInt::zero(); deg_c + 1];
 
     // perform polynomial multiplication
     for i in 0..=deg_a {
@@ -142,7 +207,8 @@ pub fn poly_mul(a: &Vec<BigUint>, b: &Vec<BigUint>) -> Vec<BigUint> {
     c
 }
 
-/// Converts a BigUint to a Field Element
-pub fn big_uint_to_fp<F: ScalarField>(big_uint: &BigUint) -> F {
-    F::from_str_vartime(&big_uint.to_str_radix(10)[..]).unwrap()
+/// Converts a BigInt to a Field Element
+pub fn big_int_to_fp<F: ScalarField>(big_int: &BigInt) -> F {
+    // if the big int is positive, convert it directly to a field element
+    F::from_str_vartime(&big_int.to_str_radix(10)[..]).unwrap()
 }
